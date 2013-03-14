40a67662854b6ecbed40871640b47038eb321ec3
Edward Cree <ec429@cantab.net>
2013-03-14 21:21:07 +0000

    gensave: make A/c IDs more stable in the face of irrelevant save changes

--- gensave.py
+++ gensave.py
@@ -11,9 +11,9 @@ def multiply(line):
 				return n*[line[l+1:]]
 	return [line]
 
-def genids(line, i, j):
+def genids(line, i):
 	if line.endswith(',NOID\n'):
-		z = '_'.join((salt, str(i), str(j), line))
+		z = '_'.join((salt, str(i), line))
 		ha = zlib.crc32(z) & 0xffffffff
 		h = hex(ha)[2:]
 		return '%s,%s\n' % (line[:-6], h.zfill(8))
@@ -29,8 +29,8 @@ if '--salt' in sys.argv:
 		sys.stderr.write('--salt requires argument!\n')
 		sys.exit(1)
 
-for i,line in enumerate(sys.stdin.readlines()):
+for line in sys.stdin.readlines():
 	lines = multiply(line)
-	for j,line in enumerate(lines):
-		line = genids(line, i, j)
+	for i,line in enumerate(lines):
+		line = genids(line, i)
 		sys.stdout.write(line)
