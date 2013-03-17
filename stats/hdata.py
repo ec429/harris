267554ad8913a3d244c8e0fe869a61438a03189e
Edward Cree <ec429@cantab.net>
2013-03-17 16:02:39 +0000

    stats/hdata: improved the Table.__str__ method to produce a proper table

--- stats/hdata.py
+++ stats/hdata.py
@@ -5,14 +5,23 @@ The hdata module provides access to the Harris game data from the files
 in 'dat/'.
 """
 
+import sys
 from hhist import date
 
 class BadDataLine(Exception): pass
 class ExcessValues(BadDataLine): pass
 
+class Counter(object):
+	def __init__(self, value):
+		self.value = value
+	def next(self, text):
+		self.value += 1
+		return self.value - 1
+
 class Table(object):
 	def __init__(self, keys):
-		self.keys = keys
+		counter = Counter(0)
+		self.keys = [('i', counter.next)] + keys
 		self.data = []
 	def read(self, f):
 		for line in f:
@@ -21,12 +30,14 @@ class Table(object):
 			if line.lstrip().startswith('#'): continue
 			self.data.append(self.parse(line))
 	def parse(self, line):
-		values = line.split(':', len(self.keys))
+		values = [''] + line.split(':', len(self.keys))
 		if len(values) > len(self.keys):
 			raise ExcessValues(values, self.keys)
 		return {key: parse(value) for ((key, parse), value) in zip(self.keys, values)}
-	def __str__(self):
-		return str(self.data)
+	def __unicode__(self):
+		widths = {k:max(len(str(k[0])), max((len(unicode(row[k[0]])) for row in self.data))) for k in self.keys}
+		return '\n'.join((' '.join((str(k[0]).ljust(widths[k]) for k in self.keys)), '\n'.join((' '.join((unicode(row[k[0]]).ljust(widths[k]) for k in self.keys)) for row in self.data))))
+	__str__ = __unicode__
 	def __repr__(self):
 		return repr(self.data)
 	def __len__(self):
@@ -114,9 +125,21 @@ Targets = Table([('name', parse_string),
 Targets.read(open('dat/targets'))
 
 if __name__ == "__main__":
-	print '%d bombers' % len(Bombers)
-	print '%d events' % len(Events)
-	print '%d fighters' % len(Fighters)
-	print '%d flaksites' % len(Flak)
-	print '%d ftrbases' % len(Ftrbases)
-	print '%d targets' % len(Targets)
+	if '--bombers' in sys.argv or '--all' in sys.argv:
+		print '%d bombers' % len(Bombers)
+		print Bombers
+	if '--events' in sys.argv or '--all' in sys.argv:
+		print '%d events' % len(Events)
+		print Events
+	if '--fighters' in sys.argv or '--all' in sys.argv:
+		print '%d fighters' % len(Fighters)
+		print Fighters
+	if '--flaksites' in sys.argv or '--all' in sys.argv:
+		print '%d flaksites' % len(Flak)
+		print Flak
+	if '--ftrbases' in sys.argv or '--all' in sys.argv:
+		print '%d ftrbases' % len(Ftrbases)
+		print Ftrbases
+	if '--targets' in sys.argv or '--all' in sys.argv:
+		print '%d targets' % len(Targets)
+		print unicode(Targets)
