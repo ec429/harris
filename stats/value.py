c2e821eaf87d7ef0d0e0ea2a69f476eec46a4881
Edward Cree <ec429@cantab.net>
2013-03-17 15:35:18 +0000

    stats/value: use the new OB event

--- stats/value.py
+++ stats/value.py
@@ -17,21 +17,21 @@ def extract_value(save):
 	days = sorted(hhist.group_by_date(save.history))
 	for d in days:
 		spend = 0
-		for i,b in enumerate(hdata.Bombers):
-			if hdata.Bombers[i]['exit'] and d[0] == hdata.Bombers[i]['exit']:
-				bcount[i] = 0
 		for h in d[1]:
 			if h['class'] == 'A':
 				if h['data']['type']['fb'] == 'B':
 					if h['data']['etyp'] == 'CT':
 						bcount[h['data']['type']['ti']] += 1
 						spend += hdata.Bombers[h['data']['type']['ti']]['cost']
-					elif h['data']['etyp'] == 'CR':
+					elif h['data']['etyp'] in ('CR', 'OB'):
 						bcount[h['data']['type']['ti']] -= 1
 			elif h['class'] == 'M':
 				if h['data']['etyp'] == 'CA':
 					cash = h['data']['data']['cash']
 					cshr = h['data']['data']['cshr']
+		for i,b in enumerate(hdata.Bombers):
+			if hdata.Bombers[i]['exit'] and d[0] == hdata.Bombers[i]['exit']:
+				assert bcount[i] == 0
 		bvalues = [b*hdata.Bombers[i]['cost'] for i,b in enumerate(bcount)]
 		cash -= spend
 		total = sum(bvalues) + cash
