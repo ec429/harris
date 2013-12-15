e62b3ed6cbbcb163c566e198cf1a194aafab6413
Edward Cree <ec429@cantab.net>
2013-12-15 08:01:29 +0000

    stats: whitespace (align '}') in extra_data

--- stats/extra_data.py
+++ stats/extra_data.py
@@ -14,28 +14,28 @@ Per industry class:
 """
 
 Bombers = {
-	"Blenheim":   {"crew":3, "short":"BLEN", "colour":'0.5',},
-	"Whitley":    {"crew":5, "short":"WHIT", "colour":'y',},
-	"Hampden":    {"crew":4, "short":"HAMP", "colour":'r',},
-	"Wellington": {"crew":6, "short":"WLNG", "colour":'c',},
-	"Manchester": {"crew":7, "short":"MANC", "colour":'m',},
-	"Stirling":   {"crew":7, "short":"STIR", "colour":'0.5',},
+	"Blenheim":   {"crew":3, "short":"BLEN", "colour":'0.5',    },
+	"Whitley":    {"crew":5, "short":"WHIT", "colour":'y',      },
+	"Hampden":    {"crew":4, "short":"HAMP", "colour":'r',      },
+	"Wellington": {"crew":6, "short":"WLNG", "colour":'c',      },
+	"Manchester": {"crew":7, "short":"MANC", "colour":'m',      },
+	"Stirling":   {"crew":7, "short":"STIR", "colour":'0.5',    },
 	"Halifax I":  {"crew":7, "short":"HAL1", "colour":'#806000',},
-	"Lancaster I":{"crew":7, "short":"LAN1", "colour":'b',},
-	"Mosquito":   {"crew":2, "short":"MOSQ", "colour":'r',},
-	"Halifax III":{"crew":7, "short":"HAL3", "colour":'y',},
-	"Lancaster X":{"crew":7, "short":"LANX", "colour":'c',},
+	"Lancaster I":{"crew":7, "short":"LAN1", "colour":'b',      },
+	"Mosquito":   {"crew":2, "short":"MOSQ", "colour":'r',      },
+	"Halifax III":{"crew":7, "short":"HAL3", "colour":'y',      },
+	"Lancaster X":{"crew":7, "short":"LANX", "colour":'c',      },
 	}
 
 Fighters = {
 	"Bf109E":{"colour":'0.6',},
 	"Bf109G":{"colour":'0.4',},
-	"Fw190A":{"colour":'r',},
-	"Do17Z": {"colour":'y',},
-	"Bf110": {"colour":'m',},
-	"Ju88C": {"colour":'g',},
-	"Do217J":{"colour":'y',},
-	"Me262": {"colour":'b',},
+	"Fw190A":{"colour":'r',  },
+	"Do17Z": {"colour":'y',  },
+	"Bf110": {"colour":'m',  },
+	"Ju88C": {"colour":'g',  },
+	"Do217J":{"colour":'y',  },
+	"Me262": {"colour":'b',  },
 	}
 
 Industries = {
