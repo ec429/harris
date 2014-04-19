06b14b9e1c659cbafbbb445c310f85662d24f6c1
Edward Cree <ec429@cantab.net>
2014-04-19 03:07:47 +0100

    stats: fix bug in fightersgraph radar line

--- stats/fightersgraph.py
+++ stats/fightersgraph.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 		gf = plt.plot_date([datum['date'].ordinal() for datum in data if hdata.inservice(datum['date'], f)], [e['fighters'][fi] for e in data if hdata.inservice(e['date'], f)], fmt='o-', color=extra[f['name']]['colour'], tz=None, xdate=True, ydate=False, label=f['name'], zorder=0)
 		if f['radpri']:
 			def i(d):
-				return hdata.inservice(d, f) and d > lbc['date']
-			gr = plt.plot_date([datum['date'].ordinal() for datum in data if i(datum['date'])], [e['radar'][fi] for e in data if i(datum['date'])], fmt='o-', color=extra[f['name']]['2colour'], tz=None, xdate=True, ydate=False, label=None, zorder=0)
+				return hdata.inservice(d, f) and d >= lbc['date']
+			gr = plt.plot_date([datum['date'].ordinal() for datum in data if i(datum['date'])], [e['radar'][fi] for e in data if i(e['date'])], fmt='o-', color=extra[f['name']]['2colour'], tz=None, xdate=True, ydate=False, label=None, zorder=0)
 	if legend: plt.legend(ncol=2, loc='upper left')
 	plt.show()
