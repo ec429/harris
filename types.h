0eea4b7993d72eeff9827f570476072186b97298
Edward Cree <ec429@cantab.net>
2014-05-02 23:55:39 +0100

    Bump copyright year to 2014

--- types.h
+++ types.h
@@ -1,7 +1,7 @@
 #pragma once
 /*
 	harris - a strategy game
-	Copyright (C) 2012-2013 Edward Cree
+	Copyright (C) 2012-2014 Edward Cree
 
 	licensed under GPLv3+ - see top of harris.c for details
 	
