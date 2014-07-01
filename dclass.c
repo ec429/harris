36ba06cebbd578644ee21b310a18e5f5cde9cf1e
Edward Cree <ec429@cantab.net>
2014-07-01 23:02:56 +0100

    dclass: correct values for DCLASS_FLAK - they're reciprocals
    Previously, selecting 'Hard' made things easier and vice-versa!

--- dclass.c
+++ dclass.c
@@ -13,5 +13,5 @@ dclass dclasses[DIFFICULTY_CLASSES]={
 	[DCLASS_ALL]   = {.name="All",                 .desc="Set all categories to the same difficulty"},
 	[DCLASS_RCITY] = {.name="Resilience (cities)", .desc="How much damage it takes to destroy a city",            .values={10, 20, 50}},
 	[DCLASS_ROTHER]= {.name="Resilience (other)",  .desc="How much damage it takes to destroy a non-city target", .values={12, 20, 36}},
-	[DCLASS_FLAK]  = {.name="Flak strength",       .desc="Strength of target flak defences and sited flak",       .values={10, 25, 40}},
+	[DCLASS_FLAK]  = {.name="Flak strength",       .desc="Strength of target flak defences and sited flak",       .values={50, 25, 15}},
 };
