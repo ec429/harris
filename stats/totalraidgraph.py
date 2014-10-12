624d0029386a7fa88589089ac126fd5823419c81
Edward Cree <ec429@cantab.net>
2014-10-12 18:04:31 +0100

    stats: add new totalraids & totalraidgraph

---
+++ stats/totalraidgraph.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+"""totalraidsgraph - graph of number of bombers deployed per night
+
+Requires matplotlib, see http://matplotlib.org or search your package
+manager (Debian: apt-get install python-matplotlib)
+"""
+
+import sys
+import hsave, hdata, totalraids
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+	legend = '--nolegend' not in sys.argv
+	save = hsave.Save.parse(sys.stdin)
+	data = totalraids.extract_totals(save, None, None)
+	pff = hdata.Events.find('id', 'PFF')
+	fig = plt.figure()
+	ax = fig.add_subplot(1,1,1)
+	dates = [key.ordinal() for key in sorted(data)]
+	tmain = [data[key][0] for key in sorted(data)]
+	tpff = [data[key][1] for key in sorted(data)]
+	ax.bar(dates, tmain, bottom=tpff, align='center', color='r', label='Main Force', linewidth=0, width=1)
+	ax.bar(dates, tpff, align='center', color='b', label='PFF', linewidth=0, width=1)
+	ax.xaxis_date()
+	if legend: plt.legend(ncol=2, loc=2)
+	plt.show()
---
+++ stats/totalraids.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+"""totalraids - number of bombers deployed per night"""
+
+import sys
+import hhist, hdata, hsave
+import optparse
+
+def daily_total(d, bombers, start, stop): # updates bombers
+	if stop:
+		return
+	totals = [0, 0] # MF,PFF
+	for h in d[1]:
+		if h['class'] == 'A':
+			acid = h['data']['acid']
+			if h['data']['type']['fb'] == 'B':
+				if h['data']['etyp'] == 'CT':
+					bombers[acid]=False
+				elif h['data']['etyp'] in ['CR', 'OB']:
+					del bombers[acid]
+				elif h['data']['etyp'] == 'RA':
+					if acid not in bombers:
+						print 'Warning: un-inited bomber %08x'%acid
+						bombers[acid] = False
+					totals[int(bombers[acid])] += 1
+				elif h['data']['etyp'] == 'PF':
+					if acid not in bombers:
+						print 'Warning: un-inited bomber %08x'%acid
+					bombers[acid] = True
+	return totals
+
+def extract_totals(save, before=None, after=None):
+	bombers = {b['id']:b['pff'] for b in save.init.bombers}
+	days = sorted(hhist.group_by_date(save.history))
+	totals = {}
+	for d in days:
+		total = daily_total(d, bombers, d[0]>=after if after else True, d[0]>=before if before else False)
+		if total is not None:
+			totals[d[0]] = total
+	return totals
+
+def parse_args(argv):
+	x = optparse.OptionParser()
+	x.add_option('-a', '--after', type='string')
+	x.add_option('-b', '--before', type='string')
+	return x.parse_args()
+
+if __name__ == '__main__':
+	opts, args = parse_args(sys.argv)
+	before = hhist.date.parse(opts.before) if opts.before else None
+	after = hhist.date.parse(opts.after) if opts.after else None
+	save = hsave.Save.parse(sys.stdin)
+	totals = extract_totals(save, before, after)
+	pff = hdata.Events.find('id', 'PFF')
+	for d in sorted(totals):
+		m,p = totals[d]
+		if m or p:
+			if pff and d < pff['date']:
+				print '%s: %d'%(d,m)
+				if p:
+					print '(Unexpected PFF: %s)'%p
+			else:
+				print '%s: %d (%d main force, %d PFF)'%(d,m+p,m,p)
