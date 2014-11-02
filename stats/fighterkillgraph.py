7bdf81a511f22e9ef0479fbe9cd5f6065f8c042c
Edward Cree <ec429@cantab.net>
2014-11-02 14:17:42 +0000

    stats: fix fighterkillgraph x-ordering

--- stats/fighterkillgraph.py
+++ stats/fighterkillgraph.py
@@ -17,11 +17,12 @@ if __name__ == '__main__':
 	data = fighterkill.extract_kills(save)
 	fig = plt.figure()
 	ax = fig.add_subplot(1,1,1)
+	dates = sorted(data.keys())
 	if showtotal:
-		gtk = plt.plot_date([d.ordinal() for d in data.keys()], [e['total']['kills'] for e in data.values()], fmt='k+-', tz=None, xdate=True, ydate=False, label='total', zorder=-2)
-		gtl = plt.plot_date([d.ordinal() for d in data.keys()], [-e['total']['losses'] for e in data.values()], fmt='k+-', tz=None, xdate=True, ydate=False, label=None, zorder=-2)
+		gtk = plt.plot_date([d.ordinal() for d in dates], [data[d]['total']['kills'] for d in dates], fmt='k+-', tz=None, xdate=True, ydate=False, label='total', zorder=-2)
+		gtl = plt.plot_date([d.ordinal() for d in dates], [-data[d]['total']['losses'] for d in dates], fmt='k+-', tz=None, xdate=True, ydate=False, label=None, zorder=-2)
 	for fi,f in enumerate(hdata.Fighters.data):
-		gfk = plt.plot_date([d.ordinal() for d in data.keys() if hdata.inservice(d, f)], [e['kills'][fi] for d,e in data.items() if hdata.inservice(d, f)], fmt='o-', mew=0, color=extra[f['name']]['colour'], tz=None, xdate=True, ydate=False, label=f['name'], zorder=0)
-		gfl = plt.plot_date([d.ordinal() for d in data.keys() if hdata.inservice(d, f)], [-e['losses'][fi] for d,e in data.items() if hdata.inservice(d, f)], fmt='o-', mew=0, color=extra[f['name']]['colour'], tz=None, xdate=True, ydate=False, label=None, zorder=0)
+		gfk = plt.plot_date([d.ordinal() for d in dates if hdata.inservice(d, f)], [data[d]['kills'][fi] for d in dates if hdata.inservice(d, f)], fmt='o-', mew=0, color=extra[f['name']]['colour'], tz=None, xdate=True, ydate=False, label=f['name'], zorder=0)
+		gfl = plt.plot_date([d.ordinal() for d in dates if hdata.inservice(d, f)], [-data[d]['losses'][fi] for d in dates if hdata.inservice(d, f)], fmt='o-', mew=0, color=extra[f['name']]['colour'], tz=None, xdate=True, ydate=False, label=None, zorder=0)
 	if legend: plt.legend(ncol=2, loc='upper left')
 	plt.show()
