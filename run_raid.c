ec429/harris

commit 5f0907329f0739ea8e03526a7d438e266e63e25e

Add notion of 'marks': mods that are only applied to new build

run_raid.c
@@ -476,7 +476,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 					dist+=d;
 					if(l<4) outward+=d;
 				}
-				unsigned int cap=types[type].capwt;
+				unsigned int cap=bstats(state->bombers[k]).capwt;
 				state->bombers[k].bombed=false;
 				state->bombers[k].crashed=false;
 				state->bombers[k].landed=false;
@@ -502,14 +502,14 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 						askill=max(askill, get_crew(state, k, l)->skill*.5);
 					}
 				}
-				state->bombers[k].speed=(types[type].speed+askill/20.0-2.0)/450.0;
+				state->bombers[k].speed=(bstats(state->bombers[k]).speed+askill/20.0-2.0)/450.0;
 				if(stream)
 				{
 					// aim for Zero Hour 01:00 plus up to 10 minutes
 					// PFF should arrive at Zero minus 6, and be finished by Zero minus 2
 					// Zero Hour is t=840, and a minute is two t-steps
 					int tt=state->bombers[k].pff?(state->raids[i].zerohour-12+irandu(8)):(state->raids[i].zerohour+irandu(20));
-					int st=tt-(outward/state->bombers[k].speed)-3;
+					int st=tt-(outward/bstats(state->bombers[k]).speed)-3;
 					if(state->bombers[k].pff) st-=3;
 					if(st<0)
 					{
@@ -523,8 +523,8 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 				startt=min(startt, state->bombers[k].startt);
 				ra_append(&state->hist, state->now, maketime(state->bombers[k].startt), state->bombers[k].id, false, state->bombers[k].type, i);
 				double eff=0.98+askill/1e3; // engineer fuel factor
-				state->bombers[k].fuelt=state->bombers[k].startt+types[type].range*0.6*eff/(double)state->bombers[k].speed;
-				unsigned int eta=state->bombers[k].startt+outward*1.1/(double)state->bombers[k].speed+12;
+				state->bombers[k].fuelt=state->bombers[k].startt+types[type].range*0.6*eff/(double)bstats(state->bombers[k]).speed;
+				unsigned int eta=state->bombers[k].startt+outward*1.1/(double)bstats(state->bombers[k]).speed+12;
 				if(!stream) eta+=36;
 				if(eta>state->bombers[k].fuelt)
 				{
@@ -539,7 +539,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 				state->bombers[k].b_in=0;
 				state->bombers[k].b_ti=0;
 				state->bombers[k].b_le=0;
-				unsigned int bulk=types[type].capbulk;
+				unsigned int bulk=bstats(state->bombers[k]).capbulk;
 				bool inext=true;
 				switch(targs[i].class)
 				{
@@ -553,7 +553,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 								if(cap>=4000) // cookie + gp+in mix
 								{
 									transfer(4000, cap, state->bombers[k].b_hc);
-									while(cap&&(bulk=types[type].capbulk-loadbulk(state->bombers[k])))
+									while(cap&&(bulk=bstats(state->bombers[k]).capbulk-loadbulk(state->bombers[k])))
 									{
 										if(inext)
 											transfer(min(bulk/1.5, 800), cap, state->bombers[k].b_in);
@@ -564,7 +564,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 								}
 								else // can't take a cookie, so just gp+in mix (but more gp than BL_USUAL)
 								{
-									while(cap&&(bulk=types[type].capbulk-loadbulk(state->bombers[k])))
+									while(cap&&(bulk=bstats(state->bombers[k]).capbulk-loadbulk(state->bombers[k])))
 									{
 										if(inext)
 											transfer(min(bulk/1.5, 300), cap, state->bombers[k].b_in);
@@ -575,15 +575,15 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 								}
 							break;
 							case BL_USUAL:
-								if(types[type].load[BL_PLUMDUFF]&&types[type].capwt>=10000&&cap>4000) // cookie + incendiaries
+								if(types[type].load[BL_PLUMDUFF]&&bstats(state->bombers[k]).capwt>=10000&&cap>4000) // cookie + incendiaries
 								{
 									transfer(4000, cap, state->bombers[k].b_hc);
-									bulk=types[type].capbulk-loadbulk(state->bombers[k]);
+									bulk=bstats(state->bombers[k]).capbulk-loadbulk(state->bombers[k]);
 									state->bombers[k].b_in=min(cap, bulk/1.5);
 								}
 								else // gp+in mix
 								{
-									while(cap&&(bulk=types[type].capbulk-loadbulk(state->bombers[k])))
+									while(cap&&(bulk=bstats(state->bombers[k]).capbulk-loadbulk(state->bombers[k])))
 									{
 										if(inext)
 											transfer(min(bulk/1.5, 800), cap, state->bombers[k].b_in);
@@ -604,7 +604,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 								}
 								// else fallthrough
 							case BL_ILLUM:
-								while(cap&&(bulk=types[type].capbulk-loadbulk(state->bombers[k])))
+								while(cap&&(bulk=bstats(state->bombers[k]).capbulk-loadbulk(state->bombers[k])))
 								{
 									if(inext)
 										transfer(min(bulk/2.0, 250), cap, state->bombers[k].b_ti);
@@ -614,7 +614,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 								}
 							break;
 							case BL_PPLUS: // LanX, up to 12,000lb cookie; LanI, up to 8,000lb cookie
-								transfer(min(types[type].capwt/5333, cap/4000)*4000, cap, state->bombers[k].b_hc);
+								transfer(min(bstats(state->bombers[k]).capwt/5333, cap/4000)*4000, cap, state->bombers[k].b_hc);
 								state->bombers[k].b_gp=cap;
 							break;
 							case BL_PONLY:
@@ -776,7 +776,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 							askill=max(askill, get_crew(state, k, l)->skill*.5);
 						}
 					}
-					if(brandp(types[type].fail/(50.0*min(240.0, 48.0+t-state->bombers[k].startt))+state->bombers[k].damage/2400.0))
+					if(brandp(bstats(state->bombers[k]).fail/(50.0*min(240.0, 48.0+t-state->bombers[k].startt))+state->bombers[k].damage/2400.0))
 					{
 						// E practise
 						for(unsigned int l=1;l<MAX_CREW;l++)
@@ -788,7 +788,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 								state->bombers[k].ld.ds=DS_MECH;
 							fa_append(&state->hist, state->now, now, state->bombers[k].id, false, state->bombers[k].type, 1);
 							state->bombers[k].failed=true;
-							if(brandp((1.0+state->bombers[k].damage/50.0)/(240.0-types[type].fail*5.0)))
+							if(brandp((1.0+state->bombers[k].damage/50.0)/(240.0-bstats(state->bombers[k]).fail*5.0)))
 							{
 								cr_append(&state->hist, state->now, now, state->bombers[k].id, false, state->bombers[k].type);
 								state->bombers[k].crashed=true;
@@ -802,7 +802,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 					while((stage<8)&&(t>RRT(7,0)||!(state->bombers[k].route[stage][0]||state->bombers[k].route[stage][1])))
 						stage=++state->bombers[k].routestage;
 					bool home=(state->bombers[k].failed&&!state->bombers[k].bombed)||(stage>=8);
-					double altitude=types[type].alt+(irandu(askill)-10)/10.0;
+					double altitude=bstats(state->bombers[k]).alt+(irandu(askill)-10)/10.0;
 					if((stage==4)&&state->bombers[k].nav[NAV_OBOE]&&xyr(state->bombers[k].lon-oboe.lon, state->bombers[k].lat-oboe.lat, 50+altitude*.3)) // OBOE
 					{
 						if(oboe.k==-1)
@@ -1000,11 +1000,11 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 							if(brandp(state->flk[i]*flakscale/min((9+targs[i].shots++)*40.0, preccap)))
 							{
 								double ddmg;
-								if(brandp(types[type].defn/800.0))
+								if(brandp(bstats(state->bombers[k]).defn/800.0))
 									ddmg=100;
 								else
 								{
-									ddmg=irandu(types[type].defn)/5.0;
+									ddmg=irandu(bstats(state->bombers[k]).defn)/5.0;
 									// Damage control; also E practise (even if ddmg==0)
 									for(unsigned int l=1;l<MAX_CREW;l++)
 										if(types[type].crew[l]==CCLASS_E)
@@ -1026,7 +1026,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 						if((x>=0)&&(y>=0)&&(x<128)&&(y<128))
 							water=lorw[x][y];
 						if(dd<(water?8*8:30*30))
-							targs[i].threat+=sqrt(targs[i].prod*types[type].capbulk/5000/(6.0+max(dd, 0.25)));
+							targs[i].threat+=sqrt(targs[i].prod*newstats(types[type]).capbulk/5000/(6.0+max(dd, 0.25)));
 					}
 					for(unsigned int i=0;i<nflaks;i++)
 					{
@@ -1046,11 +1046,11 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 							if(brandp(flaks[i].strength*flakscale/min((12+flaks[i].shots++)*40.0, preccap)))
 							{
 								double ddmg;
-								if(brandp(types[type].defn/1000.0))
+								if(brandp(bstats(state->bombers[k]).defn/1000.0))
 									ddmg=100;
 								else
 								{
-									ddmg=irandu(types[type].defn)/10.0;
+									ddmg=irandu(bstats(state->bombers[k]).defn)/10.0;
 									// Damage control; also E practise (even if ddmg==0)
 									for(unsigned int l=1;l<MAX_CREW;l++)
 										if(types[type].crew[l]==CCLASS_E)
@@ -1100,7 +1100,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 								state->fighters[j].k=k;
 						}
 					}
-					double navacc=3.0/(types[type].accu+get_crew(state, k, 0)->skill/10.0-4.0);
+					double navacc=3.0/(bstats(state->bombers[k]).accu+get_crew(state, k, 0)->skill/10.0-4.0);
 					double ex=drandu(navacc)-(navacc/2), ey=drandu(navacc)-(navacc/2);
 					state->bombers[k].driftlon=state->bombers[k].driftlon*.98+ex;
 					state->bombers[k].driftlat=state->bombers[k].driftlat*.98+ey;
@@ -1112,7 +1112,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 					clamp(state->bombers[k].lat, 0, 256);
 					state->bombers[k].navlon-=state->bombers[k].driftlon;
 					state->bombers[k].navlat-=state->bombers[k].driftlat;
-					if(state->bombers[k].nav[NAV_GEE]&&xyr(state->bombers[k].lon-gee.lon, state->bombers[k].lat-gee.lat, datebefore(state->now, event[EVENT_GEEJAM])?56+(types[type].alt*.3):gee.jrange))
+					if(state->bombers[k].nav[NAV_GEE]&&xyr(state->bombers[k].lon-gee.lon, state->bombers[k].lat-gee.lat, datebefore(state->now, event[EVENT_GEEJAM])?56+(bstats(state->bombers[k]).alt*.3):gee.jrange))
 					{
 						double geeacc=100.0/(nskill+200.0);
 						state->bombers[k].navlon=(state->bombers[k].navlon>0?1:-1)*min(fabs(state->bombers[k].navlon), geeacc);
@@ -1125,7 +1125,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 						unsigned char h=((x<128)&&(y<128))?tnav[x][y]:0;
 						wea=max(wea, h*0.08*((80+bac->skill)/120.0)-12);
 					}
-					double navp=types[type].accu*0.05*(sqrt(illum)*.8+.5)/(double)(8+max(16-wea, 8));
+					double navp=bstats(state->bombers[k]).accu*0.05*(sqrt(illum)*.8+.5)/(double)(8+max(16-wea, 8));
 					if(home&&(state->bombers[k].lon<64)) navp=1;
 					bool b=brandp(navp);
 					state->bombers[k].fix=b;
@@ -1211,8 +1211,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 							boxy=floor((state->bombers[k].lat-40)/10.0);
 						if(0<=boxx&&boxx<16&&0<=boxy&&boxy<16)
 						{
-							unsigned int type=state->bombers[k].type;
-							unsigned int size=types[type].capbulk/1000; // how big a signal on Freya ground radar
+							unsigned int size=bstats(state->bombers[k]).capbulk/1000; // how big a signal on Freya ground radar
 							// TODO: extra WINDOW (will need UI support in raid planning)
 							size*=irandu(3);
 							boxes[boxx][boxy]+=size;
@@ -1382,7 +1381,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 						{
 							if(brandp(ftypes[ft].mnv*(2.7+loadness(state->bombers[k]))/(200.0+pskill+mlskill*3)))
 							{
-								unsigned int dmg=irandu(ftypes[ft].arm)*types[bt].defn/30.0;
+								unsigned int dmg=irandu(ftypes[ft].arm)*bstats(state->bombers[k]).defn/30.0;
 								double sdc=0;
 								// Damage control; also E practise (even if dmg==0)
 								for(unsigned int l=1;l<MAX_CREW;l++)
@@ -1414,7 +1413,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 							double rgskill=0;
 							if(ng)
 								rgskill=gskill[irandu(ng)];
-							if(!types[bt].noarm&&(brandp(rgskill*0.008/types[bt].defn)))
+							if(!types[bt].noarm&&(brandp(rgskill*0.008/bstats(state->bombers[k]).defn)))
 							{
 								unsigned int dmg=irandu(20);
 								state->fighters[j].damage+=dmg;

control.c
@@ -1348,7 +1348,7 @@ screen_id control_screen(atg_canvas *canvas, game *state)
 		if(GB_btnew[i])
 			GB_btnew[i]->hidden=!datebefore(state->now, types[i].novelty);
 		if(GB_btp[i])
-			GB_btp[i]->hidden=(types[i].pribuf<8)||(state->cash<types[i].cost)||(types[i].pcbuf>=types[i].cost);
+			GB_btp[i]->hidden=(types[i].pribuf<8)||(state->cash<newstats(types[i]).cost)||(types[i].pcbuf>=newstats(types[i]).cost);
 		if(GB_btnum[i])
 		{
 			unsigned int svble=0,total=0;
@@ -2139,7 +2139,7 @@ int update_raidnums(const game *state, int seltarg)
 			}
 			else
 			{
-				unsigned int cap=types[i].capwt;
+				unsigned int cap=newstats(types[i]).capwt;
 				double dist;
 				if(stream)
 				{
@@ -2154,8 +2154,8 @@ int update_raidnums(const game *state, int seltarg)
 				{
 					dist=hypot((signed)types[i].blat-(signed)targs[seltarg].lat, (signed)types[i].blon-(signed)targs[seltarg].lon)*1.07;
 				}
-				unsigned int fuelt=types[i].range*0.6/(types[i].speed/450.0);
-				unsigned int estt=dist*1.1/(types[i].speed/450.0)+12;
+				unsigned int fuelt=types[i].range*0.6/(newstats(types[i]).speed/450.0);
+				unsigned int estt=dist*1.1/(newstats(types[i]).speed/450.0)+12;
 				if(!stream) estt+=36;
 				if(estt>fuelt)
 				{

dat/bombers
@@ -37,7 +37,7 @@
 Bristol:Blenheim:            5000:198:205: 1200:55:28: 6:60:190:96:61:          :10-01-1941:PNG    :Ab
 Armstrong-Whitworth:Whitley: 8000:165:176: 7000:70:32: 9:32:320:81:45:          :18-06-1941:PNBWG  :GEE,H2S,Ab
 Handley Page:Hampden:       10000:202:190: 4000:65:24: 8:48:265:63:40:          :02-11-1941:PNWG   :GEE,Ab
-Vickers:Wellington:         12000:190:180: 4500:85:15: 2:40:311:87:39:          :21-10-1943:PNWGGG :GEE,H2S,Ab,Pd,Us,Ar
+Vickers:Wellington:         12000:185:180: 4500:85:16: 2:40:311:87:39:          :21-10-1943:PNWGGG :GEE,H2S,Ab,Pd,Us,Ar
 # while Manc's Sv may seem high, remember that its high Fa brings this right down in practice
 Avro:Manchester:            20000:210:185:10350:64:24:16:55:325:81:45:05-11-1940:26-06-1942:PPNBWGG:GEE,H2S,OBOE,CREWBG,HEAVY,Ab,Pd,Us,Ar
 Supermarine:Southampton:    33000:230:220:16000:64:22: 6:50:265:96:61:25-12-1940:17-11-1943:PNBWGG :GEE,H2S,HEAVY,EXTRA,Ab,Pd,Us,Ar

dat/events
@@ -75,6 +75,7 @@ MOD_BBBAY:03-04-1943
 MOD_MERLIN:22-07-1943
 MOD_PROP:01-11-1943
 MOD_GLODGE:18-02-1944
+STIRLING_III:08-01-1943
 
 #RCM
 # Boozer is a receiver for enemy radar emissions.  However, in a bomber stream a Boozer warning doesn't mean much

dat/mods
@@ -1,61 +1,84 @@
 # Format: dsv
-# DESCRIPTION:ACNAME:STAT:OLD:NEW:DD-MM-YYYY
-# Description is not used in-game (except in debugging messages)
+# DESCRIPTION:ACNAME:STAT:OLD:NEW:DD-MM-YYYY:[MARK]
+# Description is not used in-game except for MARK and in debugging messages
 # ACNAME should match NAME from dat/bombers
-# Stat is one of cost, speed, alt, capwt, (capbulk), svp, defn, fail, accu, range, crew, (loads), flags
+# Stat is one of cost, speed, alt, capwt, (capbulk), svp, defn, fail, accu, range, crew, (loads), flags, nil
 #   (bracketed stats are not currently supported)
+#   nil is used with MARK to set the name of mark 0; it sets no stat
 # old is ignored by game
 # new is the new value of the stat (or, for flags, the flag to set)
+# MARK indicates this only affects new-build aircraft (only supports a limited selection of stats)
 
 # WARNING: mods which touch the same stat on the same type MUST be in date order!
 #  (The game does not check for this!)
 # So please keep this whole file in date order.
 
 # Description            A/c name    stat    old     new   DD-MM-YYYY
-Twin guns in turret     :Blenheim   :defn :     28:     27:11-01-1940
-Extra dorsal guns       :Hampden    :defn :     24:     23:11-01-1940
-Self-sealing tank       :Whitley    :defn :     32:     30:06-02-1940
-Self-sealing tank       :Wellington :defn :     15:     14:29-02-1940
-Self-sealing tank       :Hampden    :defn :     23:     22:21-03-1940
-Self-sealing tank       :Blenheim   :defn :     27:     25:04-05-1940
-Electric bomb release   :Whitley    :accu :     32:     34:10-08-1940
-Crew rationalisation    :Stirling   :crew :PPNWEGG:PNBWEGG:18-04-1941
-Crew rationalisation    :Manchester :crew :PPNBWGG:PNBWEGG:22-04-1941
-Broughton shadow factory:Wellington :cost :  12000:   8000:30-07-1941
-Remove beam guns        :Halifax I  :speed:    225:    227:09-09-1941
-Remove beam guns        :Halifax I  :alt  :    200:    202:09-09-1941
-Engine derating         :Manchester :fail :     16:     14:14-09-1941
-Engine derating         :Manchester :speed:    210:    206:14-09-1941
-Engine derating         :Manchester :capwt:  10350:   9000:14-09-1941
-Engine derating         :Manchester :svp  :     64:     66:14-09-1941
-Exactor priming         :Stirling   :svp  :     40:     45:21-10-1941
-Exhaust shrouds         :Halifax I  :defn :     22:     20:02-12-1941
-Exhaust shrouds         :Halifax I  :fail :      6:      8:02-12-1941
-Exhaust shrouds         :Halifax I  :speed:    227:    216:02-12-1941
-Exhaust shrouds         :Halifax I  :alt  :    202:    184:02-12-1941
-Exhaust shrouds         :Halifax I  :range:    335:    320:02-12-1941
-Bulbous rudder          :Halifax I  :defn :     20:     16:15-01-1942
-Remove exhaust shrouds  :Halifax I  :defn :     16:     18:12-03-1942
-Remove exhaust shrouds  :Halifax I  :fail :      8:      6:12-03-1942
-Remove exhaust shrouds  :Halifax I  :speed:    216:    227:12-03-1942
-Remove exhaust shrouds  :Halifax I  :alt  :    184:    202:12-03-1942
-Remove exhaust shrouds  :Halifax I  :range:    320:    335:12-03-1942
-Type XIV Bomb Sight     :Lancaster I:accu :     60:     72:19-04-1942
-Overload tanks          :Halifax I  :flags:       :OVLTANK:09-05-1942
-Type XIV Bomb Sight     :Halifax I  :accu :     62:     68:27-06-1942
-Remove nose turret      :Halifax I  :speed:    227:    230:21-08-1942
-Remove nose turret      :Halifax I  :range:    335:    340:21-08-1942
-Remove nose turret      :Halifax I  :crew :PNWEGGG:PNBWEGG:21-08-1942
-Type XIV Bomb Sight     :Southampton:accu :     50:     62:14-10-1942
-Type XIV Bomb Sight     :Stirling   :accu :     50:     56:07-11-1942
-T-1 Bomb Sight          :Wellington :accu :     40:     52:13-03-1943
-Bulged Bomb Bay         :Mosquito   :capwt:   2000:   4000:03-04-1943
-Bulged Bomb Bay         :Mosquito   :capbulk: 2000:   4000:03-04-1943
-Bulged Bomb Bay         :Mosquito   :loads:       :Po     :03-04-1943
-Bulged Bomb Bay         :Mosquito   :loads:       :Hh     :03-04-1943
-Two-stage Merlin        :Mosquito   :alt  :    300:    320:22-07-1943
-Two-stage Merlin        :Mosquito   :range:    390:    400:22-07-1943
-Two-stage Merlin        :Mosquito   :speed:    325:    340:22-07-1943
-Paddleblade prop        :Lancaster I:speed:    232:    236:01-11-1943
-Paddleblade prop        :Lancaster I:alt  :    225:    230:01-11-1943
-Gransden Lodge          :Lancaster I:defn :     12:     11:18-02-1944
+Mk IV                   :Blenheim   :nil  :       :       :00-00-0000:0
+Mk IV                   :Whitley    :nil  :       :       :00-00-0000:0
+Mk I                    :Hampden    :nil  :       :       :00-00-0000:0
+Mk Ia                   :Wellington :nil  :       :       :00-00-0000:0
+Mk I                    :Manchester :nil  :       :       :00-00-0000:0
+Mk I                    :Southampton:nil  :       :       :00-00-0000:0
+Mk I                    :Stirling   :nil  :       :       :00-00-0000:0
+Mk I                    :Elswick    :nil  :       :       :00-00-0000:0
+Mk IV                   :Mosquito   :nil  :       :       :00-00-0000:0
+Mk I                    :Windsor    :nil  :       :       :00-00-0000:0
+Mk I                    :Selkirk    :nil  :       :       :00-00-0000:0
+Mk V                    :Whitley    :speed:    165:    170:00-00-0000:1
+Twin guns in turret     :Blenheim   :defn :     28:     27:11-01-1940:
+Extra dorsal guns       :Hampden    :defn :     24:     23:11-01-1940:
+Self-sealing tank       :Whitley    :defn :     32:     30:06-02-1940:
+Self-sealing tank       :Wellington :defn :     16:     15:29-02-1940:
+Self-sealing tank       :Hampden    :defn :     23:     22:21-03-1940:
+Mk Ic                   :Wellington :defn :     15:     14:02-04-1940:1
+Mk Ic                   :Wellington :speed:    185:    190:02-04-1940:1
+Self-sealing tank       :Blenheim   :defn :     27:     25:04-05-1940:
+Electric bomb release   :Whitley    :accu :     32:     34:10-08-1940:
+Crew rationalisation    :Stirling   :crew :PPNWEGG:PNBWEGG:18-04-1941:
+Crew rationalisation    :Manchester :crew :PPNBWGG:PNBWEGG:22-04-1941:
+Mk III                  :Wellington :speed:    190:    200:17-06-1941:2
+Mk III                  :Wellington :alt  :    180:    185:17-06-1941:2
+Mk III                  :Wellington :defn :     14:     13:17-06-1941:2
+Broughton shadow factory:Wellington :cost :  12000:   8000:30-07-1941:
+Remove beam guns        :Halifax I  :speed:    225:    227:09-09-1941:
+Remove beam guns        :Halifax I  :alt  :    200:    202:09-09-1941:
+Engine derating         :Manchester :fail :     16:     14:14-09-1941:
+Engine derating         :Manchester :speed:    210:    206:14-09-1941:
+Engine derating         :Manchester :capwt:  10350:   9000:14-09-1941:
+Engine derating         :Manchester :svp  :     64:     66:14-09-1941:
+Exactor priming         :Stirling   :svp  :     40:     45:21-10-1941:
+Exhaust shrouds         :Halifax I  :defn :     22:     20:02-12-1941:
+Exhaust shrouds         :Halifax I  :fail :      6:      8:02-12-1941:
+Exhaust shrouds         :Halifax I  :speed:    227:    216:02-12-1941:
+Exhaust shrouds         :Halifax I  :alt  :    202:    184:02-12-1941:
+Exhaust shrouds         :Halifax I  :range:    335:    320:02-12-1941:
+Bulbous rudder          :Halifax I  :defn :     20:     16:15-01-1942:
+Remove exhaust shrouds  :Halifax I  :defn :     16:     18:12-03-1942:
+Remove exhaust shrouds  :Halifax I  :fail :      8:      6:12-03-1942:
+Remove exhaust shrouds  :Halifax I  :speed:    216:    227:12-03-1942:
+Remove exhaust shrouds  :Halifax I  :alt  :    184:    202:12-03-1942:
+Remove exhaust shrouds  :Halifax I  :range:    320:    335:12-03-1942:
+Type XIV Bomb Sight     :Lancaster I:accu :     60:     72:19-04-1942:
+Overload tanks          :Halifax I  :flags:       :OVLTANK:09-05-1942:
+Type XIV Bomb Sight     :Halifax I  :accu :     62:     68:27-06-1942:
+Remove nose turret      :Halifax I  :speed:    227:    230:21-08-1942:
+Remove nose turret      :Halifax I  :range:    335:    340:21-08-1942:
+Remove nose turret      :Halifax I  :crew :PNWEGGG:PNBWEGG:21-08-1942:
+Type XIV Bomb Sight     :Southampton:accu :     50:     62:14-10-1942:
+Type XIV Bomb Sight     :Stirling   :accu :     50:     56:07-11-1942:
+Mk III                  :Stirling   :speed:    200:    205:08-01-1943:1
+Mk III                  :Stirling   :alt  :    175:    185:08-01-1943:1
+Mk III                  :Stirling   :svp  :     45:     55:08-01-1943:1
+Mk III                  :Stirling   :defn :     18:     17:08-01-1943:1
+T-1 Bomb Sight          :Wellington :accu :     40:     52:13-03-1943:
+Bulged Bomb Bay         :Mosquito   :capwt:   2000:   4000:03-04-1943:
+Bulged Bomb Bay         :Mosquito   :capbulk: 2000:   4000:03-04-1943:
+Bulged Bomb Bay         :Mosquito   :loads:       :Po     :03-04-1943:
+Bulged Bomb Bay         :Mosquito   :loads:       :Hh     :03-04-1943:
+Two-stage Merlin        :Mosquito   :alt  :    300:    320:22-07-1943:
+Two-stage Merlin        :Mosquito   :range:    390:    400:22-07-1943:
+Two-stage Merlin        :Mosquito   :speed:    325:    340:22-07-1943:
+Paddleblade prop        :Lancaster I:speed:    232:    236:01-11-1943:
+Paddleblade prop        :Lancaster I:alt  :    225:    230:01-11-1943:
+Gransden Lodge          :Lancaster I:defn :     12:     11:18-02-1944:

dat/texts
@@ -296,6 +296,12 @@ A modification has been applied to your Lancaster Mark I aircraft.  Known as the
 This mod is not applicable to the Mark X Lancasters, as they have the Rose twin .50 caliber turret in place of
  the Fraser-Nash FN120 quad-.303 of the Mark I.
 
+==STIRLING_III==
+
+Short Stirling production has switched over to the Mark III upgrade, which brings slight increases in speed and
+ ceiling altitude, slightly increasing survivability.  Also, the troublesome 'Exactor' controls are replaced by
+ ordinary wire controls which will hopefully prove easier to maintain.
+
 ==BOOZER==
 
 A new radio countermeasure, codenamed "Boozer", is now being installed into your bombers.  It detects the

gensave.py
@@ -12,11 +12,11 @@ def multiply(line):
 	return [line]
 
 def genids(line, i):
-	if line.endswith(',NOID\n'):
+	if ',NOID' in line:
 		z = '_'.join((salt, str(i), line))
 		ha = zlib.crc32(z) & 0xffffffff
 		h = hex(ha)[2:].rstrip('L')
-		return '%s,%s\n' % (line[:-6], h.zfill(8))
+		return line.replace('NOID', h.zfill(8))
 	else:
 		return line
 

globals.c
@@ -187,7 +187,7 @@ int set_init_state(game *state)
 	state->roe.idtar=true;
 	for(unsigned int i=0;i<MAXMSGS;i++)
 		state->msg[i]=NULL;
-	
+
 	state->hist.nents=0;
 	state->hist.nalloc=0;
 	state->hist.ents=NULL;

commit b3f46751b3d75298360e1c9f7d6ace71a35c770a

Fix bugs in speed handling introduced with the marks code
We were using bstats(bomber).speed, rather than bomber.speed. for calculating
 ETAs and fuel states (endurance).  The latter includes some variation, and
 is scaled to different units.  The result of this was that bombers were
 always turning for home before they reached the target.

Fixes: 5f0907329f07 ("Add notion of 'marks': mods that are only applied to new build")

run_raid.c
@@ -509,7 +509,7 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 					// PFF should arrive at Zero minus 6, and be finished by Zero minus 2
 					// Zero Hour is t=840, and a minute is two t-steps
 					int tt=state->bombers[k].pff?(state->raids[i].zerohour-12+irandu(8)):(state->raids[i].zerohour+irandu(20));
-					int st=tt-(outward/bstats(state->bombers[k]).speed)-3;
+					int st=tt-(outward/state->bombers[k].speed)-3;
 					if(state->bombers[k].pff) st-=3;
 					if(st<0)
 					{
@@ -523,8 +523,8 @@ screen_id run_raid_screen(atg_canvas *canvas, game *state)
 				startt=min(startt, state->bombers[k].startt);
 				ra_append(&state->hist, state->now, maketime(state->bombers[k].startt), state->bombers[k].id, false, state->bombers[k].type, i);
 				double eff=0.98+askill/1e3; // engineer fuel factor
-				state->bombers[k].fuelt=state->bombers[k].startt+types[type].range*0.6*eff/(double)bstats(state->bombers[k]).speed;
-				unsigned int eta=state->bombers[k].startt+outward*1.1/(double)bstats(state->bombers[k]).speed+12;
+				state->bombers[k].fuelt=state->bombers[k].startt+types[type].range*0.6*eff/(double)state->bombers[k].speed;
+				unsigned int eta=state->bombers[k].startt+outward*1.1/(double)state->bombers[k].speed+12;
 				if(!stream) eta+=36;
 				if(eta>state->bombers[k].fuelt)
 				{