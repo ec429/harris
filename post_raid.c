ec429/harris

commit 75645ddfa8f44cea1f55774a654610be872119bf

Drop supporting research if no longer needed for unpicked tech
When picking tech at the end of the month, in the situation where
 tech A is picked, tech B's date is tomorrow, and tech C is
 Supporting Research (for B), next month B will no longer be future
 and thus won't need Supporting Research.  Thus, slot C must be
 emptied.
Without this fix, the Supporting Research cannot be removed by the
 user and will thus tie up that slot until it is picked.

post_raid.c
@@ -915,18 +915,32 @@ mothball:
 		}
 		state->researching[slot]=NULL;
 		apply_techs(&builder->entities, &builder->tn);
-		for(unsigned int i=0;i<NNAVAIDS;i++)
+		for(i=0;i<NNAVAIDS;i++)
 			if(builder->tn.na[i])
 				event[navevent[i]]=(date){1, 1, 1};
 		if(builder->tn.na[NAV_GEE]>1)
 			event[EVENT_ALLGEE]=(date){1, 1, 1};
 		// re-realise all bombers in case any doctrine has changed
-		for(unsigned int i=0;i<state->ndesigns;i++)
+		for(i=0;i<state->ndesigns;i++)
 		{
 			struct bomber *b=state->designs+i;
 			if(b->proto_work>=b->tproto&&b->prod_work>=b->tprod)
 				realise_design(b);
 		}
+		// if a tech that wasn't picked just stopped being future, remove support
+		for(i=0;i<3;i++)
+			if(tech_future(state->now, state->researching[i])&&!tech_future(tomorrow, state->researching[i]))
+			{
+				unsigned int j;
+				for(j=0;j<3;j++)
+					if(state->researching[j]==&supporting)
+					{
+						state->researching[j]=NULL;
+						break;
+					}
+				if(j>=3)
+					fprintf(stderr, "no support found to remove!\n");
+			}
 	}
 	for(unsigned int ev=0;ev<NEVENTS;ev++)
 	{

commit adb614ca5edfba6a320d4f66a29d392e23779889

Fix null-dereference crash in end-of-month tech processing
Fixes: 75645ddfa8f4 ("Drop supporting research if no longer needed for unpicked tech")

post_raid.c
@@ -929,7 +929,7 @@ mothball:
 		}
 		// if a tech that wasn't picked just stopped being future, remove support
 		for(i=0;i<3;i++)
-			if(tech_future(state->now, state->researching[i])&&!tech_future(tomorrow, state->researching[i]))
+			if(state->researching[i]&&tech_future(state->now, state->researching[i])&&!tech_future(tomorrow, state->researching[i]))
 			{
 				unsigned int j;
 				for(j=0;j<3;j++)